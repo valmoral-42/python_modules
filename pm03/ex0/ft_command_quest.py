3569ec7895109da70dd11ad0e675486588d1e6d3
Valentina Morales <valmoral@c3r6s3.42urduliz.com>
2026-05-19 18:33:50 +0200

    00

--- pm03/ex0/ft_command_quest.py
+++ pm03/ex0/ft_command_quest.py
@@ -2,7 +2,17 @@ import sys
 
 
 def command() -> None:
+    argc = len(sys.argv)
+    i = 0
     print(f"Program name: {sys.argv[0]}")
+    if len == 1:
+        print("No arguments provided!")
+    else:
+        print(f"Arguments received {argc - 1}")
+        for i < argc:
+            print(f"Argument {i}: {sys.argv[i]}")
+            i =+ 1
+    print(f"Total arguments: {argc}")
 
 
 if __name__ == "__main__":
