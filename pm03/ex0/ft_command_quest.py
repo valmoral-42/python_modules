1c95f9ccb8872fbb8d1224f84bba97bb02f3c63e
Valentina Morales <valmoral@c3r6s3.42urduliz.com>
2026-05-20 16:56:04 +0200

    0_and_1

--- pm03/ex0/ft_command_quest.py
+++ pm03/ex0/ft_command_quest.py
@@ -3,12 +3,11 @@ import sys
 
 def command() -> None:
     argc = len(sys.argv)
-    i = 0
     print(f"Program name: {sys.argv[0]}")
     if argc == 1:
         print("No arguments provided!")
     else:
-        print(f"Arguments received {argc - 1}")
+        print(f"Arguments received: {argc - 1}")
         for i in range(1, argc):
             print(f"Argument {i}: {sys.argv[i]}")
     print(f"Total arguments: {argc}")
---
+++ pm03/ex1/ft_score_analytics.py
@@ -0,0 +1,35 @@
+import sys
+
+
+def list_score() -> list:
+    score = []
+    for i in range(1, len(sys.argv)):
+        try:
+            score.append(int(sys.argv[i]))
+        except ValueError:
+            print(f"Invalid parameter: '{sys.argv[i]}'")
+    return score
+
+
+def score_analytics() -> None:
+    if len(sys.argv) == 1:
+        print("No scores provided. Usage: python3 ft_score_analytics.py "
+              "<score1> <score2> ...")
+        return
+    score = list_score()
+    if len(score) == 0:
+        print("No scores provided. Usage: python3 ft_score_analytics.py "
+              "<score1> <score2> ...")
+    else:
+        print(f"Scores processed: {score}")
+        print(f"Total players: {len(score)}")
+        print(f"Total score: {sum(score)}")
+        print(f"Average score: {(sum(score)/len(score))}")
+        print(f"High score: {max(score)}")
+        print(f"Low score: {min(score)}")
+        print(f"Score range: {(max(score)-(min(score)))}")
+
+
+if __name__ == "__main__":
+    print("=== Player Score Analytics ===")
+    score_analytics()
