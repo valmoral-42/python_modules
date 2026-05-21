a97ed7faac73c87031a0ab8db82438d883c6ccc1
Valentina Morales <valmoral@c4r6s6.42urduliz.com>
2026-05-21 19:26:19 +0200

    5

--- pm03/ex5/ft_data_stream.py
+++ pm03/ex5/ft_data_stream.py
@@ -2,9 +2,9 @@ import typing
 import random
 
 
-def gen_event(i: int) -> typing.Generator[tuple[str, str], None, None]:
+def gen_event() -> typing.Generator[tuple[str, str], None, None]:
     players = ["alice", "bob", "charlie", "dylan"]
-    actions = ["run", "eat", "sleep", "grab"
+    actions = ["run", "eat", "sleep", "grab",
                "move", "climb", "swim", "release"]
 
     while True:
@@ -13,10 +13,26 @@ def gen_event(i: int) -> typing.Generator[tuple[str, str], None, None]:
         yield (player, action)
 
 
+def consume_event(prev_list: list[tuple[str, str]]) -> None:
+    new_list = prev_list.copy()
+    random.shuffle(prev_list)
+    for event in prev_list:
+        print(f"Got event from list: {event}")
+        new_list.remove(event)
+        print(f"Remains in list: {new_list}")
+
+
 def data_stream() -> None:
     for i in range(5):
         event = next(gen_event())
         print(f"Event {i}: Player {event[0]} did action {event[1]}")
+
+    list_10 = []
+    for i in range(5):
+        list_10.append(next(gen_event()))
+    print(f"Built list of 10 events: {list_10}")
+
+    consume_event(list_10)
 
 
 if __name__ == "__main__":
