7f0578ed92108de76725b3108909c96772a7d474
Valentina Morales <valmoral@c3r7s2.42urduliz.com>
2026-06-01 16:36:27 +0200

    0106

---
+++ pm04/ex0/ft_ancient_text.py
@@ -0,0 +1,35 @@
+import sys
+
+
+def text_recovery() -> None:
+
+    argc = len(sys.argv)
+    if argc != 2:
+        print("Usage: ft_ancient_text.py <file>")
+        print("")
+        return
+    else:
+        file_name = sys.argv[1]
+        print(f"Accessing file '{file_name}'")
+
+        try:
+            vault = open(file_name, "r")
+            print("---")
+            print("")
+
+            file_content = vault.read()
+            print(f"{file_content}")
+            print("---")
+
+            vault.close()
+            print(f"File '{file_name}' closed.")
+
+        except FileNotFoundError as e:
+            print(f"Error opening file '{file_name}': {e}")
+            print("Data not saved.")
+            print("")
+
+
+if __name__ == "__main__":
+    print("=== Cyber Archives Recovery ===")
+    text_recovery()
